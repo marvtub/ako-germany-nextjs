c4c1aa41dd743cb09731b9a7775cc6f3b5589b33
marvtub <marvinaziz35@gmail.com>
2022-01-05 12:23:56 +0100

    added better logos

--- components/customerLogoSection.js
+++ components/customerLogoSection.js
@@ -1,13 +1,19 @@
+import Image from 'next/image'
+import mcdonalds from "../public/mcdonalds.png"
+import walmart from "../public/walmart.png"
+import engie from "../public/engie.png"
+import nestle from "../public/nestle.png"
+
 function CustomerLogoSection() {
   const logos = [
-    { href: "/1200px-Engie_logo.svg.png", id: 1 },
-    { href: "/McDonalds-logo-1968.png", id: 2 },
-    { href: "/Walmart-Logo.png", id: 3 },
-    { href: "/nestle-logo-black-and-white.png", id: 4 },
+    { href: engie, id: 1 },
+    { href: mcdonalds, id: 2 },
+    { href: walmart, id: 3 },
+    { href: nestle, id: 4 },
   ];
   console.log(logos);
   return (
-    <div className=" pt-10 pb-5 bg-slate-0">
+    <div className=" pt-10 pb-2 md:pb-5 bg-slate-0">
       <div className="flex flex-col justify-center mx-auto max-w-screen-2xl ">
         <div className="text-xl text-center text-gray-700 ">
           Mit über <span className="text-rose-700">300+</span> zufriedenen
@@ -15,8 +21,8 @@ function CustomerLogoSection() {
         </div>
         <div className="flex flex-wrap justify-center gap-5 mt-10 md:justify-around">
           {logos.map((e) => (
-            <div className="pt-2 text-gray-400 dark:text-gray-400">
-              <img src={e.href} width={100} height={100} alt=""></img>
+            <div className="pt-2 text-gray-400 dark:text-gray-400 md:w-1/12 w-2/12">
+              <Image src={e.href}  quality="30" alt={e.href} placeholder="blur" key={e.id} />
             </div>
           ))}
         </div>
