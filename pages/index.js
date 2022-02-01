2f787f3dc7d3e8aa483229f761a89cd1f74bf84c
marvtub <marvinaziz35@gmail.com>
2022-02-01 10:27:12 +0100

    removed yarn from project and added favicon

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,8 @@ export default function Home() {
     <div className="" >
       <Head>
         <title>AKO Germany | Temperaturüberwachung leicht gemacht</title>
-        <meta name="description" content="AKO Germany bietet erstklassige " />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="AKO Germany bietet erstklassige Technologien zur automatischen Überwachung von Produkten während der Lagerung und des Transports." />
+        <link rel="icon" href="/AKO-Germany-icon.png" sizes="32x32" />
       </Head>
       <Hero />
       <CustomerLogoSection />
